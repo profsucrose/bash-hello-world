6f0105af277d2a3cd3b2096b055c2fc91d65b711
profsucrose <dr.blobbles@gmail.com>
2020-04-17 00:09:26 -0700

    First commit

---
+++ hello_world.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+MESSAGE="Hello World"
+DELAY=0.1
+last_color=-1
+
+while [ 1 ]
+do
+	current_color=$((RANDOM % 5 + 1))
+	if [ $current_color -eq $last_color ]
+	then
+		continue
+	fi
+	last_color=$current_color
+	echo "$(tput setaf $current_color)"$MESSAGE"$(tput sgr0)"
+	sleep $DELAY
+	clear
+done
